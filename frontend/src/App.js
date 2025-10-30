d2f0a34823448567e27615e8c4e3aecd6c2cae17
Albert Minkh <minkhalbert@yandex.ru>
2025-10-30 10:35:27 +0700

    added changes for running django admin panel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,6 +27,7 @@ function App() {
   return (
     <BrowserRouter>
         <Routes>
+          <Route path="/admin/*" element={null} />
           <Route path='/' element={<Layout />}>
             <Route path="login" element={<Login />} />
             <Route path="register" element={<Register />} />
