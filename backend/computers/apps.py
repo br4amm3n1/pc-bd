05e0d7ae2c7c58887618ada42418f3b8fae8dc3f
Minkh Albert <minkhalbert@yandex.ru>
2026-04-09 16:20:04 +0700

    connected signals

--- backend/computers/apps.py
+++ backend/computers/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ComputersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'computers'
+
+    def ready(self):
+        import computers.signals
